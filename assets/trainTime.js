b9ad7453ea3722d8c15ab356b437a60804da5bfa
Norton Nice <nortonnice@gmail.com>
2018-09-29 20:02:57 -0700

    first commit

---
+++ assets/trainTime.js
@@ -0,0 +1,21 @@
+var trainRef = firebase.database().ref("trains/");
+
+trainRef.set ({
+        Steamer: {
+            destination: "Boston",
+            startTime: 0100, 
+            frequency: 60
+    },
+
+        Cruiser: {
+            destination: "Provencetown",
+            startTime: 0230,
+            frequency: 120
+        },
+        Streamliner: {
+            destination: "Los Angeles",
+            startTime: 2359,
+            frequency: 720
+
+        }
+});
